97abe1fac4ff070bbeb08670b26f4ded517fd254
Alexander Zotov <azotov86@gmail.com>
2020-04-12 22:14:47 +0400

    add connect to the database

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const app = require('./src/app');
+const db = require('./src/models');
 
 const port = 9000;
 const hostName = '0.0.0.0';
@@ -6,3 +7,17 @@ const hostName = '0.0.0.0';
 app.listen(port, hostName, () => {
   console.log(`Server running at http://${hostName}:${port}/`);
 });
+
+function databaseConnect() {
+  db.sequelize
+    .authenticate()
+    .then(() => {
+      console.log('Connection to the database has been established successfully.');
+    })
+    .catch(err => {
+      console.log(`Unable to connect to the database: ${err}`);
+      setTimeout(databaseConnect, 10000);
+    });
+}
+
+databaseConnect();
