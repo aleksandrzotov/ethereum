79536597b2c3c3f6108b28591e2f0f42c783fcd3
Alexander Zotov <azotov86@gmail.com>
2020-04-15 15:18:24 +0400

    getBlocksData fucntion improve

--- src/utils/index.js
+++ src/utils/index.js
@@ -15,29 +15,36 @@ function constructGetBlockUrl(tag, apiKey) {
   return `https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&tag=${tag}&boolean=true&apikey=${apiKey}`;
 }
 
+async function getPackOfBlocks(tags) {
+  return Promise.all(
+    tags.map(tag => {
+      const url = constructGetBlockUrl(tag, API_KEY);
+      return getData(url);
+    })
+  ).catch(e => {
+    console.log('getLastBlocksData error', e);
+    throw e;
+  });
+}
+
 async function getBlocksData(tags) {
   // On a large amount of blocks, the API starts to
   // return a message that the request limit has been reached:
-  // "Max rate limit reached"
-  // Otherwise, it would be convenient to use Promise.all like this:
+  // "Max rate limit reached".
+  // So I broke the request into stages.
 
-  // return Promise.all(
-  //   tags.map(tag => {
-  //     const url = `https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&tag=${tag}&boolean=true&apikey=${API_KEY}`;
-  //     return getData(url);
-  //   })
-  // ).catch(e => {
-  //   console.log('getLastBlocksData error', e);
-  //   throw e;
-  // });
+  let blocks = [];
+  const packSize = 10;
 
-  const blocks = [];
-  for (let i = 0; i < tags.length; i++) {
-    const url = constructGetBlockUrl(tags[i], API_KEY);
-    const block = await getData(url);
-    blocks.push(block);
-    console.log(`Block with number ${i + 1} from ${tags.length} was received.`);
+  for (let i = 0; i < tags.length; i += packSize) {
+    const curTags = tags.slice(i, i + packSize);
+    const curBlocks = await getPackOfBlocks(curTags);
+    console.log(
+      `Blocks with number ${i + 1} - ${i + curTags.length} from ${tags.length} was received.`
+    );
+    blocks = blocks.concat(curBlocks);
   }
+
   return blocks;
 }
 
